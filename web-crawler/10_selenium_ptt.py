3146db3adc0f00e8b3a31c4f9403d15fdc2e7eb4
uuboyscy <aegis12321@gmail.com>
2025-05-18 15:23:40 +0800

    selenium with options

--- web-crawler/10_selenium_ptt.py
+++ web-crawler/10_selenium_ptt.py
@@ -4,7 +4,16 @@ from selenium.webdriver.common.by import By
 
 url = "https://www.ptt.cc/bbs/index.html"
 
-driver = webdriver.Chrome()
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("disable-extensions")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("window-size=1080,720")
+chrome_options.add_argument("--ignore-certificate-errors")
+chrome_options.add_argument("--allow-insecure-localhost")
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get(url)
 
