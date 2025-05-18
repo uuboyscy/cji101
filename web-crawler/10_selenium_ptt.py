c044754afe9d44bf3678aa1aa873468fc7c08fde
uuboyscy <aegis12321@gmail.com>
2025-05-18 15:20:26 +0800

    selenium ptt practice

--- web-crawler/10_selenium_ptt.py
+++ web-crawler/10_selenium_ptt.py
@@ -1,5 +1,4 @@
-import time
-
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -17,6 +16,20 @@ driver.find_element(By.PARTIAL_LINK_TEXT, value=title).click()
 button_xpath = "/html/body/div[2]/form/div[1]/button"
 driver.find_element(By.XPATH, value=button_xpath).click()
 
-time.sleep(5)
+# print(driver.get_cookies())
+cookies = {
+    cookie_set["name"]: cookie_set["value"]
+    for cookie_set in driver.get_cookies()
+}
+print(cookies)
+
+# time.sleep(5)
 
 driver.quit()
+
+res = requests.get(
+    url="https://www.ptt.cc/bbs/Gossiping/index.html",
+    headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"},
+    cookies=cookies,
+)
+print(res.text)
